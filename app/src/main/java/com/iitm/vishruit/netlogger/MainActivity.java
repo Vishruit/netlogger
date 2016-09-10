ee4a411c5880a38476e02e1ef2ad22180d4b0f81
Vishruit <princepaulite@gmail.com>
2016-09-10 19:21:35 +0530

    Code Simplified

--- app/src/main/java/com/iitm/vishruit/netlogger/MainActivity.java
+++ app/src/main/java/com/iitm/vishruit/netlogger/MainActivity.java
@@ -63,11 +63,10 @@ public class MainActivity extends AppCompatActivity {
                 if (gps.canGetLocation()) {
                     double latitude = gps.getLatitude();
                     double longitude = gps.getLongitude();
-                    String type = getNetworkClass(getApplicationContext());
                     Connectivity ck = new Connectivity();
                     Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude +
                             "\nLong: " + longitude +
-                            "\nType: " + ck.getNetworkType(MainActivity.this), // type,
+                            "\nType: " + ck.getNetworkType(MainActivity.this),
                             Toast.LENGTH_LONG).show();
                 } else {
                     // Ask user to enable GPS/network in settings by throwing pop-up
@@ -177,33 +176,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public String getNetworkClass(Context context) {
-
-        TelephonyManager mTelephonyManager = (TelephonyManager)
-                context.getSystemService(Context.TELEPHONY_SERVICE);
-        int networkType = mTelephonyManager.getNetworkType();
-
-        switch (networkType) {
-            case TelephonyManager.NETWORK_TYPE_GPRS:
-            case TelephonyManager.NETWORK_TYPE_EDGE:
-            case TelephonyManager.NETWORK_TYPE_CDMA:
-            case TelephonyManager.NETWORK_TYPE_1xRTT:
-            case TelephonyManager.NETWORK_TYPE_IDEN:
-                return "2G";
-            case TelephonyManager.NETWORK_TYPE_UMTS:
-            case TelephonyManager.NETWORK_TYPE_EVDO_0:
-            case TelephonyManager.NETWORK_TYPE_EVDO_A:
-            case TelephonyManager.NETWORK_TYPE_HSDPA:
-            case TelephonyManager.NETWORK_TYPE_HSUPA:
-            case TelephonyManager.NETWORK_TYPE_HSPA:
-            case TelephonyManager.NETWORK_TYPE_EVDO_B:
-            case TelephonyManager.NETWORK_TYPE_EHRPD:
-            case TelephonyManager.NETWORK_TYPE_HSPAP:
-                return "3G";
-            case TelephonyManager.NETWORK_TYPE_LTE:
-                return "4G";
-            default:
-                return "Unknown";
-        }
-    }
 }
